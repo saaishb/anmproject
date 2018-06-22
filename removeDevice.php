2bfd00f221e325a03588f55a30f25c190cdd1432
saaishb <saaishbhonagiri33@gmail.com>
2018-06-22 08:42:55 +0000

    commiting from VM

---
+++ removeDevice.php
@@ -0,0 +1,28 @@
+<?php
+include('config.php');
+try{
+	$file_db->exec("CREATE TABLE IF NOT EXISTS listdevices (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,ip TEXT,port INTEGER,community TEXT,version TEXT,firstprobe TEXT,latestprobe TEXT,fail INTEGER)");}
+catch(PDOException $e) {
+    echo $e->getMessage();
+}
+$ip = $_GET['ip'];
+$port = $_GET['port'];
+$community = $_GET['community'];
+$version = $_GET['version'];
+#echo $ip.$port.$community.$version;
+
+if(empty($ip) || empty($port)||empty($community) || empty($version)) {
+	echo "FALSE";
+}
+else{
+	$x = $file_db->exec("DELETE FROM listdevices WHERE ip='$ip' AND port='$port'AND community='$community' AND version='$version'");
+
+	if(!$x){
+		echo "FALSE";
+	}
+	else{
+		echo "OK";
+	}
+}
+$file_db=null;
+?>
