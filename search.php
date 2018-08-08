3af0cdb2530ca3f635127f38f76d60b150cce4e8
saaishb <saaishbhonagiri33@gmail.com>
2018-08-08 14:21:42 +0000

    search without wildcards

---
+++ search.php
@@ -0,0 +1,30 @@
+<?php
+include('config.php');
+$mac = $_GET['mac'];
+$result = $file_db->query("SELECT * FROM list where macs='$mac'");
+$array = array();
+foreach ($result as $result) {
+		$out = $result['ip'].'|'.$result['vlan'].'|'.$result['port'].'|'.$result['macs'];
+		if (in_array($out, $array)){
+			
+		}
+		else{
+			array_push($array, $out);
+		}
+}
+
+if(empty($array)){
+	$res = $file_db->query("SELECT * FROM listdevices");
+	$count = 0;
+	foreach ($res as $res) {
+		$count = $count+1;
+	}
+	print "No match in $count devices.";
+}
+else{
+foreach ($array as $elem) {
+	print $elem."\n";
+}
+}
+
+?>
